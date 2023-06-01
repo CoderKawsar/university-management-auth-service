5cebbf6af23a49bcb5fb8409ced1d1bd03355f7e
Kawsar Ahmed <coder.kawsar@gmail.com>
2023-06-01 22:40:20 +0600

    create user service, controller, route, utils created

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,6 @@
 import express, { Application, Request, Response } from 'express'
 import cors from 'cors'
+import userRoutes from '../src/app/modules/users/users.route'
 
 const app: Application = express()
 
@@ -9,6 +10,9 @@ app.use(express.urlencoded({ extended: true }))
 
 // use cors
 app.use(cors())
+
+// application routes
+app.use('/api/v1/users', userRoutes)
 
 // testing
 app.get('/', (req: Request, res: Response) => {
---
+++ src/app/modules/users/users.controller.ts
@@ -0,0 +1,25 @@
+import { Request, Response } from 'express'
+import userService from './users.service'
+
+const createUser = async (req: Request, res: Response) => {
+  try {
+    const { user } = req.body
+
+    const result = await userService.createUserService(user)
+    return res.status(200).json({
+      success: true,
+      message: 'User created successfully!',
+      data: result,
+    })
+  } catch (error) {
+    res.status(400).json({
+      success: false,
+      message: 'Failed to create user!',
+      data: error,
+    })
+  }
+}
+
+export default {
+  createUser,
+}

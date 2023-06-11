86878abaf7e0520ee603154d6b238718644cd4b1
Kawsar Ahmed <coder.kawsar@gmail.com>
2023-06-11 17:34:13 +0600

    Dynamic sorting done

--- src/app/modules/academicSemester/academicSemester.service.ts
+++ src/app/modules/academicSemester/academicSemester.service.ts
@@ -1,5 +1,7 @@
 import status from 'http-status'
+import { SortOrder } from 'mongoose'
 import ApiError from '../../../errors/ApiError'
+import { paginationHelpers } from '../../../helpers/paginationHelper'
 import { IGenericResponse } from '../../../interfaces/common'
 import { IPaginationOptions } from '../../../interfaces/pagination'
 import { academicSemesterTitleCodeMapper } from './academicSemester.constant'
@@ -19,11 +21,18 @@ const createSemester = async (
 const getAllSemesters = async (
   paginationOptions: IPaginationOptions
 ): Promise<IGenericResponse<IAcademicSemester[]>> => {
-  const { page = 1, limit = 10 } = paginationOptions
+  const { page, limit, skip, sortBy, sortOrder } =
+    paginationHelpers.calculatePagination(paginationOptions)
 
-  const skip = (page - 1) * limit
+  const sortConditions: { [key: string]: SortOrder } = {}
+  if (sortBy && sortOrder) {
+    sortConditions[sortBy] = sortOrder
+  }
 
-  const result = await AcademicSemester.find({}).sort().skip(skip).limit(limit)
+  const result = await AcademicSemester.find({})
+    .sort(sortConditions)
+    .skip(skip)
+    .limit(limit)
   const total = await AcademicSemester.countDocuments()
 
   return {
